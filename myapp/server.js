de75bd77484c3566dad435bc592b1bae4a2ee3b4
Zoey Frakes <zfrakes811@gmail.com>
2020-04-27 15:45:01 -0400

    updated server.js

--- myapp/server.js
+++ myapp/server.js
@@ -15,7 +15,15 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "");
+mongoose
+.connect(process.env.MONGODB_URI, {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log(`DB Connection Error: ${err.message}`);
+});
 
 // Start the API server
 app.listen(PORT, function() {
